b2a6bba157df2def3c6b390e653b646592d68b92
GeonHwan Cha <geonhwan.cha@clroot.io>
2025-09-22 10:12:07 +0900

    Revert "test(domain): SearchCriteria 테스트 추가 및 구현 개선"
    This reverts commit bcf803914161fa67b8d3e3933db02f08b5d05e38.

--- domain/src/main/kotlin/io/clroot/ball/domain/port/SearchCriteria.kt
+++ domain/src/main/kotlin/io/clroot/ball/domain/port/SearchCriteria.kt
@@ -1,18 +1,5 @@
 package io.clroot.ball.domain.port
 
-import kotlin.reflect.KProperty1
-
 interface SearchCriteria {
-    fun isEmpty(): Boolean =
-        this::class
-            .members
-            .filterIsInstance<KProperty1<*, *>>()
-            .filter { it.returnType.isMarkedNullable }
-            .all {
-                if (it.call(this) is SearchCriteria?) {
-                    (it.call(this) as SearchCriteria?)?.isEmpty() ?: true
-                } else {
-                    it.call(this) == null
-                }
-            }
+    fun isEmpty(): Boolean
 }
